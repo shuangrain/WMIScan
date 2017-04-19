2988b26af3e69599b37f624db354c40232049e32
shuangrain <shuangrain07@gmail.com>
2017-04-19 23:01:36 +0800

    Initi

---
+++ WMIScan/MainForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WMIScan
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnScan = new System.Windows.Forms.Button();
+            this.list = new System.Windows.Forms.ListBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnScan
+            // 
+            this.btnScan.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnScan.Location = new System.Drawing.Point(20, 20);
+            this.btnScan.Margin = new System.Windows.Forms.Padding(5);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(191, 78);
+            this.btnScan.TabIndex = 0;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // list
+            // 
+            this.list.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.list.FormattingEnabled = true;
+            this.list.ItemHeight = 20;
+            this.list.Location = new System.Drawing.Point(20, 108);
+            this.list.Margin = new System.Windows.Forms.Padding(5);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(431, 304);
+            this.list.TabIndex = 1;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnDelete.Location = new System.Drawing.Point(260, 20);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(5);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(191, 78);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "刪除所選";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 437);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.btnScan);
+            this.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WMIScan v1.0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ WMIScan/MainForm.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+
+namespace WMIScan
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("本程式提供簡單的掃描WMI是否含有HAO123的入侵，並提供刪除功能 !!");
+            sb.AppendFormat("GitHub: https://github.com/shuangrain/WMIScan");
+
+            MessageBox.Show(sb.ToString());
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            ManagementObjectCollection queryCollection = getList();
+
+            action(() =>
+            {
+                Dictionary<string, string> dty = new Dictionary<string, string>();
+                int idx = 0;
+                foreach (ManagementObject mo in queryCollection)
+                {
+                    string name = mo["Name"].ToString();
+                    string scriptText = mo["ScriptText"].ToString();
+                    string url = string.Empty;
+
+                    if (scriptText.Contains("http"))
+                    {
+                        int first = scriptText.IndexOf("http") - 1;
+                        if (scriptText[first] != '"')
+                        {
+                            //跳出
+                            break;
+                        }
+                        int end = first;
+                        while (end < (scriptText.Length - 1))
+                        {
+                            if (scriptText[end] == '"')
+                            {
+                                //跳出
+                                break;
+                            }
+
+                            end++;
+                        }
+                        if (scriptText[end] != '"')
+                        {
+                            //跳出
+                            break;
+                        }
+                        first++;
+                        end--;
+
+                        url = scriptText.Substring(first, end);
+                    }
+                    if (!string.IsNullOrWhiteSpace(url))
+                    {
+                        string id = string.Join("", ((byte[])mo["CreatorSID"]).Select(x => x.ToString()));
+                        idx++;
+                        dty.Add(id, $"{idx}: {name} | {url}");
+                    }
+                }
+
+                if (dty.Count > 0)
+                {
+                    list.DataSource = new BindingSource(dty, null); ;
+
+                    list.DisplayMember = "Value";
+                    list.ValueMember = "Key";
+                }
+                else
+                {
+                    list.DataSource = null;
+                }
+            }, "Not Found !!");
+        }
+
+        private ManagementObjectCollection getList()
+        {
+            ManagementScope ms = new ManagementScope(@"\root\cimv2");
+            ms.Connect();
+
+            ObjectQuery oq = new ObjectQuery("SELECT * FROM ActiveScriptEventConsumer");
+            ManagementObjectSearcher query = new ManagementObjectSearcher(ms, oq);
+            ManagementObjectCollection queryCollection = query.Get();
+
+            return queryCollection;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (list.Items.Count > 0 && list.SelectionMode == SelectionMode.One)
+            {
+                string id = ((KeyValuePair<string, string>)list.SelectedItem).Key;
+                ManagementObjectCollection queryCollection = getList();
+                action(() =>
+                {
+                    foreach (ManagementObject mo in queryCollection)
+                    {
+                        string tmp = string.Join("", ((byte[])mo["CreatorSID"]).Select(x => x.ToString()));
+                        MessageBox.Show(tmp + "\r\n" + id);
+                        if (tmp == id)
+                        {
+                            mo.Delete();
+                        }
+                    }
+
+                    MessageBox.Show("Success !!");
+                }, "Error !!");
+            }
+        }
+
+        private void action(Action action, string errorMsg)
+        {
+            try
+            {
+                action();
+            }
+            catch
+            {
+                MessageBox.Show(errorMsg);
+            }
+        }
+    }
+}
